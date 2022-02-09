f431aa68a7930696f2ca0efe8262f07aac34dd36
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-02-09 22:24:12 +0900

    [FEAT] Regex에 맞을 때 Label이 Print되도록 구현

--- TexBrother_MVVM_Study_Public/TexBrother_MVVM_Study/ViewModel/SecondViewModel.swift
+++ TexBrother_MVVM_Study_Public/TexBrother_MVVM_Study/ViewModel/SecondViewModel.swift
@@ -12,47 +12,71 @@ import RxCocoa
 // MARK: - SecondViewModel
 
 final class SecondViewModel {
-  struct Input {
-    let emailText: Observable<String>
-    let passwordText: Observable<String>
-    let nickNameText: Observable<String>
-    let registerBtnClicked: Observable<Void>
-    let privacyAgree: Observable<Bool>
-    let promotionAgree: Observable<Bool>
-  }
-  struct Output {
-    let registerResult: Observable<RegisterModel>
-    let registerEnabled: Observable<Bool>
-  }
+    struct Input {
+        let emailText: Observable<String>
+        let passwordText: Observable<String>
+        let nickNameText: Observable<String>
+        let registerBtnClicked: Observable<Void>
+        let privacyAgree: Observable<Bool>
+        let promotionAgree: Observable<Bool>
+    }
+    struct Output {
+        let registerResult: Observable<RegisterModel>
+        let registerEnabled: Observable<Bool>
+    }
 }
 extension SecondViewModel {
-
-  func transform (input: Input) -> Output {
-      let registerModel = Observable.combineLatest(input.emailText,
-                                                   input.passwordText,
-                                                   input.nickNameText,
-                                                   input.privacyAgree,
-                                                   input.promotionAgree)
-        .map {email, password, nickname, privacy, promotion -> RegisterModel in
-          let temp = RegisterModel(email: email,
-                                   passWord: password,
-                                   nickName: nickname,
-                                   privacy: privacy,
-                                   promotion: promotion)
-          return temp
-        }
-      
-      // TODO - validate using regex
-      // HINT - CombineLatest, flatMapLatest
-//
-//      let enabled =
-//      let register =
-      let temp = RegisterModel(email: "email",
-                               passWord: "password",
-                               nickName: "nickname",
-                               privacy: true,
-                               promotion: true)
-      return .init(registerResult: Observable.just(temp), registerEnabled: Observable.just(true))
-
-  }
+    
+    func transform (input: Input) -> Output {
+        let registerModel = Observable.combineLatest(input.emailText,
+                                                     input.passwordText,
+                                                     input.nickNameText,
+                                                     input.privacyAgree,
+                                                     input.promotionAgree)
+            .map {email, password, nickname, privacy, promotion -> RegisterModel in
+                let temp = RegisterModel(email: email,
+                                         passWord: password,
+                                         nickName: nickname,
+                                         privacy: privacy,
+                                         promotion: promotion)
+                return temp
+            }
+        
+        // TODO - validate using regex
+        // HINT - CombineLatest, flatMapLatest
+        
+        let enabled = Observable.combineLatest(input.emailText,
+                                               input.passwordText,
+                                               input.nickNameText,
+                                               input.privacyAgree,
+                                               input.promotionAgree)
+            .map { email, password, nickname, privacy, promotion -> Bool in
+                let emailPattern = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,20}$"
+                let passwordPattern = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,50}"
+                let emailRegex = try? NSRegularExpression(pattern: emailPattern)
+                let passwordRegex = try? NSRegularExpression(pattern: passwordPattern)
+                
+                if let _ = emailRegex?.firstMatch(in: email, options: [], range: NSRange(location: 0, length: email.count)),
+                   let _ = passwordRegex?.firstMatch(in: password, options: [], range: NSRange(location: 0, length: password.count)) {
+                    if nickname.count > 1 {
+                        return true
+                    }
+                }
+                
+                return false
+            }
+        
+        let register = Observable.combineLatest(registerModel, enabled)
+            .map { model, enable -> RegisterModel in
+                
+                if enable {
+                    return model
+                }
+                
+                return RegisterModel(email: "", passWord: "", nickName: "", privacy: false, promotion: false)
+            }
+        
+        return .init(registerResult: register, registerEnabled: enabled)
+        
+    }
 }
